58f8914946cb702921c65597ce3869cb98c51e29
yutchaii <yuthapichai14531@gmail.com>
2017-11-08 13:59:28 +0700

    Complete chat layout

--- application/views/default/sideMenu.php
+++ application/views/default/sideMenu.php
@@ -1,91 +1,7 @@
 <aside class="main-sidebar">
 	<!-- sidebar: style can be found in sidebar.less -->
 	<section class="sidebar">
-		<div class="chat-box">
-			<button data-toggle="collapse" data-target="#chat-box" type="button" class="btn btn-primary btn-lg chat-button"><i class="fa fa-commenting" data-toggle='chat-box'></i><span>ติดต่อ</span></button>
-			<div id="chat-box" class="box box-primary direct-chat direct-chat-primary collapse ">
-				<div class="box-header with-border">
-					<h3 class="box-title">ติดต่อ-แจ้งปัญหา</h3>
-					<div class="box-tools pull-right">
-						<button type="button" class="btn btn-box-tool" data-target="#chat-box" data-toggle="collapse" ><i class="fa fa-times"></i></button>
-					</div>
-				</div>
-				<!-- /.box-header -->
-				<div class="box-body">
-				<!-- Conversations are loaded here -->
-				<div class="direct-chat-messages">
-					<!-- Message. Default to the left -->
-					<div class="direct-chat-msg">
-						<div class="direct-chat-info clearfix">
-							<span class="direct-chat-name pull-left">Alexander Pierce</span>
-							<span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
-						</div>
-						<!-- /.direct-chat-info -->
-						<img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
-						<div class="direct-chat-text">
-							Is this template really for free? That's unbelievable!
-						</div>
-					<!-- /.direct-chat-text -->
-					</div>
-					<!-- /.direct-chat-msg -->
-
-					<!-- Message to the right -->
-					<div class="direct-chat-msg right">
-					<div class="direct-chat-info clearfix">
-						<span class="direct-chat-name pull-right">Sarah Bullock</span>
-						<span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
-					</div>
-					<!-- /.direct-chat-info -->
-					<img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image"><!-- /.direct-chat-img -->
-					<div class="direct-chat-text">
-						You better believe it!
-					</div>
-					<!-- /.direct-chat-text -->
-					</div>
-					<!-- /.direct-chat-msg -->
-				</div>
-				<!--/.direct-chat-messages-->
-
-				<!-- Contacts are loaded here -->
-				<div class="direct-chat-contacts">
-					<ul class="contacts-list">
-					<li>
-						<a href="#">
-						<img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="User Image">
-
-						<div class="contacts-list-info">
-								<span class="contacts-list-name">
-								Count Dracula
-								<small class="contacts-list-date pull-right">2/28/2015</small>
-								</span>
-							<span class="contacts-list-msg">How have you been? I was...</span>
-						</div>
-						<!-- /.contacts-list-info -->
-						</a>
-					</li>
-					<!-- End Contact Item -->
-					</ul>
-					<!-- /.contatcts-list -->
-				</div>
-				<!-- /.direct-chat-pane -->
-				</div>
-				<!-- /.box-body -->
-				<div class="box-footer">
-				<form action="#" method="post">
-					<div class="input-group">
-					<input type="text" name="message" placeholder="Type Message ..." class="form-control">
-						<span class="input-group-btn">
-							<button type="submit" class="btn btn-primary btn-flat">Send</button>
-						</span>
-					</div>
-				</form>
-				</div>
-				<!-- /.box-footer-->
-			</div>
-		</div>
-
 		<ul class="sidebar-menu">
-			
 			<!-- Main menu -->
 			<li class="header">เมนูหลัก</li>
 
@@ -256,10 +172,3 @@
 </section>
 <!-- /.sidebar -->
 </aside>
-
-<script>
-	$(document).ready(function() 
-	{
-		
-	});
-</script>
